c9b253e6e83209998732f3ed59273130f7c712de
HildaGD <hilda.guevara20@gmail.com>
2020-06-27 17:08:15 -0600

    build

---
+++ build/precache-manifest.308768d16a9ba8036aece03de59313cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea21d5029981ff580ed84334ca95c1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f8c5b2cde7a35a350f",
+    "url": "/static/css/2.97d3883c.chunk.css"
+  },
+  {
+    "revision": "aca81cb727c45c0cbfc4",
+    "url": "/static/css/main.3af9de88.chunk.css"
+  },
+  {
+    "revision": "10f8c5b2cde7a35a350f",
+    "url": "/static/js/2.f4f67ff2.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.f4f67ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca81cb727c45c0cbfc4",
+    "url": "/static/js/main.019c79d5.chunk.js"
+  },
+  {
+    "revision": "930e8c0ff13fb0219d7e",
+    "url": "/static/js/runtime-main.b3860de1.js"
+  }
+]);
